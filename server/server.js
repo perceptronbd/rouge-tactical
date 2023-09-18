8126f485038c03a480634e1d5dce60360aeb82bd
kowshikjawad <jawadkowshik@gmail.com>
2023-09-18 23:23:14 +0600

    connect mongoDB cluster

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
 const app = express();
-const PORT = process.env.BASE_URL;
+const PORT = process.env.PORT || 5000
 
 
 
@@ -11,6 +13,16 @@ app.get("/", (req, res) => {
   
 });
 
-app.listen(PORT, () => {
-  console.log(`server running at ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`server running at ${PORT}`);
+// });
+
+
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            app.listen(PORT, () =>{
+                console.log(`Server running on port ${PORT}`)
+            })
+        })
+
+        .catch((err) => console.log(err))
