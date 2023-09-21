980332a33894fbe34cf97e2a2866c72129d54de9
Parasite-231 <muktadir@iut-dhaka.edu>
2023-09-21 21:38:00 +0600

    fixed folder struct and tested db connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.BASE_URL;
+const DB_CONNECT_LINK = process.env.DB_URL;
 
 
-
+mongoose
+  .connect(DB_CONNECT_LINK) 
+  .then(() => {
+    console.log("DB Connected");
+    
+  })
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send(":)");
-  
 });
 
 app.listen(PORT, () => {
