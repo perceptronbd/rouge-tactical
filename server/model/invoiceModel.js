21f1b0b0d1000aeac0f005bd65bd3a6382a2f9b8
Parasite-231 <muktadir@iut-dhaka.edu>
2023-09-23 11:00:41 +0600

    added invoice model

---
+++ server/model/invoiceModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Vendor = require("./vendorModel");
+
+const invoiceSchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+  },
+  invoiceNumber: {
+    type: String,
+    required: true,
+  },
+  vendor: {
+    type: Schema.Types.ObjectId,
+    ref: Vendor,
+    required: true,
+  },
+  item: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+  totalAmount: {
+    type: String,
+    required: true,
+  },
+  depositAmount: {
+    type: String,
+    required: true,
+  },
+  delivered: {
+    type: Boolean,
+    default: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Invoice = mongoose.model("Invoice", invoiceSchema);
+
+module.exports = Invoice;
