97cd6c9fefcb0f020696956b11a6f079219a7bf3
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-23 17:41:07 +0600

    remove server folder

--- server/controller/userController.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler = require("express-async-handler");
-const User = require("../model/userModel");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-
-require("dotenv").config()
