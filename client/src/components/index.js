faa0a1b3b900da72928be33f12f4c3383a4841c9
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-23 18:56:33 +0600

    Create SelectInput.jsx

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@
 export { CheckBox } from "./inputs/CheckBox";
 export { FormInput } from "./inputs/FormInput";
 export { SearchInput } from "./inputs/SearchInput";
+export { SelectInput } from "./inputs/SelectInput";
