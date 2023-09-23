625a62b8f3fb2a5dbab5cc658b34900e897787c8
Parasite-231 <muktadir@iut-dhaka.edu>
2023-09-23 10:44:10 +0600

    addeD REQUIRED FIELD

--- server/model/docModel.js
+++ server/model/docModel.js
@@ -5,19 +5,24 @@ const Schema = mongoose.Schema;
 const docSchema = new Schema({
   originalname: {
     type: String,
+    required: true,
   },
   mimetype: {
     type: String,
+    required: true,
   },
   size: {
     type: String,
+    required: true,
   },
   uploadedBy: {
     userId: { type: Schema.Types.ObjectId, ref: "User" },
     name: String,
+    required: true,
   },
   filePath: {
     type: String,
+    required: true,
   },
   createdAt: {
     type: Date,
