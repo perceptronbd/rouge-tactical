f6b3f5265ae64dba508b18022b60129a1642d1d3
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-24 01:33:19 +0600

    update accent color

--- client/src/components/inputs/SelectInput.jsx
+++ client/src/components/inputs/SelectInput.jsx
@@ -21,12 +21,11 @@ export const SelectInput = (props) => {
         {...inputProps}
         onChange={onChange}
         className={cw(
-          `peer block border rounded-lg w-72 p-2 focus:outline-none focus:ring-1 focus:border-accent-secondary `,
+          `peer block border rounded-lg w-72 p-2 focus:outline-none focus:ring-1 focus:border-accent-tertiary `,
           className
         )}
       >
         <optgroup label={label} className="font-semibold text-sm">
-          {/* Add a default "Select" option */}
           <option
             value=""
             key="select-option"
@@ -46,7 +45,7 @@ export const SelectInput = (props) => {
       {label && (
         <label
           htmlFor={id}
-          className="absolute text-sm px-1 text-gray-500 duration-300 transform -translate-y-6 bg-foreground scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-accent-secondary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:bg-white"
+          className="absolute text-sm px-1 text-gray-500 duration-300 transform -translate-y-6 bg-foreground scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-accent-tertiary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:bg-white"
         >
           {label}
         </label>
