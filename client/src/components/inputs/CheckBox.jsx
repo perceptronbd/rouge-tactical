682eeff361d204eaf33c004a645f586fa8c5017d
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-24 01:19:07 +0600

    fix accent color

--- client/src/components/inputs/CheckBox.jsx
+++ client/src/components/inputs/CheckBox.jsx
@@ -7,7 +7,7 @@ export const CheckBox = (props) => {
     <>
       <div className="py-1">
         <input
-          className="appearance-none h-4 w-4 border-2 border-accent-secondary rounded bg-transparent checked:bg-accent-secondary checked:border-accent-secondary focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          className="appearance-none h-4 w-4 border-2 border-accent-tertiary rounded bg-transparent checked:bg-accent-tertiary checked:border-accent-tertiary focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
           type="checkbox"
           id={id}
           {...inputProps}
