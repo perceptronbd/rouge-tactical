0c25330d1345cd07c0c7a615d948e1eeb7b3b959
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-24 01:17:12 +0600

    isolate components

--- client/src/components/Components.jsx
+++ client/src/components/Components.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { CheckBox, FormInput, SearchInput, SelectInput } from "../components";
 
 export const Components = () => {
   return (
-    <div className="bg-foreground h-[95vh] flex justify-center items-center m-4 rounded-md p-4"></div>
+    <div className="bg-foreground h-[95vh] flex flex-col gap-5 justify-center items-center m-4 rounded-md p-4">
+      <CheckBox label="Checkbox" />
+      <FormInput label="Form Input" />
+      <SearchInput label="Search Input" />
+      <SelectInput
+        label="Select Input"
+        selectOpts={[
+          { id: 1, name: "Option 1" },
+          { id: 2, name: "Option 2" },
+        ]}
+      />
+    </div>
   );
 };
