7acb8a9303338097666df1e0a31759c1eafeb075
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-24 01:13:49 +0600

    Create isolation environment

--- client/src/components/Components.jsx
+++ client/src/components/Components.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const Components = () => {
-  return <div>Components</div>;
+  return (
+    <div className="bg-foreground h-[95vh] flex justify-center items-center m-4 rounded-md p-4"></div>
+  );
 };
