85641a4cda0892bbf7e8635d048c6d9dbe70b952
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-24 21:05:22 +0600

    cleared the mess

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { Checkbox } from "./inputs/Checkbox";
+export { FormInput } from "./inputs/FormInput";
+export { SearchInput } from "./inputs/SearchInput";
+export { SelectInput } from "./inputs/SelectInput";
