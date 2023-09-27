e190c14b1805ff906c710e6e959d9036a50c3da3
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-28 01:59:44 +0600

    modify font size

--- client/src/components/inputs/FormInput.jsx
+++ client/src/components/inputs/FormInput.jsx
@@ -18,7 +18,7 @@ export const FormInput = (props) => {
       {label && (
         <label
           htmlFor={id}
-          className="absolute text-sm px-1 text-gray-500 duration-300 transform -translate-y-1.5 bg-white top-4 z-10 origin-[0] left-2.5 peer-focus:text-accent-tertiary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:bg-white"
+          className="absolute text-base px-1 text-gray-500 duration-300 transform -translate-y-1.5 bg-white top-4 z-10 origin-[0] left-2.5 peer-focus:text-accent-tertiary peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1 peer-focus:scale-75 peer-focus:-translate-y-6 peer-focus:bg-white"
         >
           {label}
         </label>
