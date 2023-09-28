b4b022a51f190c457dc3824d8c49adcd96e0d469
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-29 02:30:35 +0600

    add icon to the button if passed through parameter

--- client/src/components/buttons/Button.jsx
+++ client/src/components/buttons/Button.jsx
@@ -8,12 +8,13 @@ export const Button = ({
   className,
   rounded,
   disabled = false,
+  icon: Icon,
   ...props
 }) => {
   return (
     <button
       className={cw(
-        `px-4 py-2 rounded m-0 my-4 h-10 ${className}`,
+        `flex justify-center items-center gap-x-2 px-4 py-2 3xl:p-1 rounded m-0 my-4 h-10 ${className}`,
         {
           "bg-accent-tertiary text-white": variant === "default" || !variant,
           "bg-yellow-500 text-black": variant === "warning",
@@ -33,6 +34,9 @@ export const Button = ({
       disabled={disabled}
       {...props}
     >
+      <span className="flex justify-center items-center">
+        {Icon && <Icon className="w-5 h-5" />}
+      </span>
       {children}
     </button>
   );
