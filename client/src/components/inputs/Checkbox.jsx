9359450478d289e64d03f4769866a14d1d4dec1b
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-30 03:45:38 +0600

    modify UI text color for checked state

--- client/src/components/inputs/Checkbox.jsx
+++ client/src/components/inputs/Checkbox.jsx
@@ -7,12 +7,15 @@ export const Checkbox = (props) => {
     <>
       <div className="py-1">
         <input
-          className="appearance-none h-4 w-4 border-2 border-accent-tertiary rounded bg-transparent checked:bg-accent-tertiary checked:border-accent-tertiary focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          className="peer appearance-none h-4 w-4 border-2 border-accent-tertiary rounded bg-transparent checked:bg-accent-tertiary checked:border-accent-tertiary focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
           type="checkbox"
           id={id}
           {...inputProps}
         />
-        <label className="inline-block" htmlFor={id}>
+        <label
+          className="inline-block peer-checked:text-accent-tertiary peer-checked:font-semibold transition-all ease-in-out duration-300"
+          htmlFor={id}
+        >
           {label}
         </label>
       </div>
