c895dfc7ff3e1db87e917c922481d1d84565b3a8
Asif Vudi <asifaslam.aa81@gmail.com>
2023-09-30 01:00:30 +0600

    modify to recieve style

--- client/src/components/views/Container.jsx
+++ client/src/components/views/Container.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { cw } from "../../utils";
 
-export const Container = ({ children }) => {
+export const Container = ({ children, className }) => {
   return (
-    <div className="bg-foreground h-[98vh] w-full flex flex-col gap-5 justify-center items-center rounded-md p-4">
+    <div
+      className={cw(
+        "bg-foreground h-[98vh] w-full flex flex-col gap-5 justify-center items-center rounded-md p-4",
+        className
+      )}
+    >
       {children}
     </div>
   );
