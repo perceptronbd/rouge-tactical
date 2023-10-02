88d406ac2e059e6605665b23d1f69a565b715863
Asif Vudi <asifaslam.aa81@gmail.com>
2023-10-03 00:57:56 +0600

    remove bg color and change radius

--- client/src/components/tabs/TabPanel.jsx
+++ client/src/components/tabs/TabPanel.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export const TabPanel = ({ children, isActive }) => {
   return isActive ? (
-    <div className="bg-foreground h-[94%] flex justify-center items-center">
-      {children}
-    </div>
+    <div className=" h-[94%] flex justify-center items-center">{children}</div>
   ) : null;
 };
