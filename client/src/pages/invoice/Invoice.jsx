52cefbc0ba91d85a3b373cd0ee39e962fac7683e
Asif Vudi <asifaslam.aa81@gmail.com>
2023-10-12 22:52:26 +0600

    - fix vendor select option - render selected vendor - add loading state

--- client/src/pages/invoice/Invoice.jsx
+++ client/src/pages/invoice/Invoice.jsx
@@ -1,31 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { MdPostAdd } from "react-icons/md";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { Button } from "../../components";
 import { Table } from "./Table";
 import { data } from "../../mock/invoice";
 import { vendorData } from "../../mock/vendor";
-import { Button } from "../../components";
+import { capitalizeFirstWord } from "../../utils/capitalize";
 
 export const Invoice = () => {
   const [selectedVendor, setSelectedVendor] = useState(null);
+  const [vendorDetails, setVendorDetails] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      const vendor = vendorData.find((vendor) => vendor.id === selectedVendor);
+
+      setLoading(false);
+      setVendorDetails(vendor);
+      console.log(vendorDetails);
+    }, 1000);
+  }, [selectedVendor, vendorDetails]);
 
   const handleVendorChange = (event) => {
-    setSelectedVendor(event.target.value);
-    const vendor = vendorData.find(
-      (vendor) => vendor.id === event.target.value
-    );
-    console.log(event.target.value, vendor);
+    setSelectedVendor(parseInt(event.target.value));
   };
 
   return (
     <section className="bg-foreground w-full h-full p-4 rounded">
       <section className="h-70 mb-2">
-        <div className="flex items-center gap-2 mb-2">
+        <div className="flex items-center gap-6 mb-2">
           <div>
             <select
               id="vendor"
               value={selectedVendor}
               onChange={handleVendorChange}
-              className="peer block border rounded-lg w-72 p-2 focus:outline-none focus:ring-1 focus:border-accent-tertiary "
+              className="peer block border rounded-lg w-64 p-2 focus:outline-none focus:ring-1 focus:border-accent-tertiary "
             >
               <option value="" className="text-textColor-light">
                 Select Vendor
@@ -47,37 +59,46 @@ export const Invoice = () => {
               Select Vendor
             </label>
           </div>
+
           <Button className={"w-10 m-0"} variant={"ghost"}>
             +
           </Button>
         </div>
-        <div className="px-4 py-2 flex flex-col gap-2 w-fit border rounded">
-          <div className="flex gap-4">
-            <p className="w-16 text-textColor-light">Contact</p>
-            <p>{selectedVendor}</p>
+        {loading ? (
+          <div className="w-80 h-52 3xl:h-56 border rounded flex justify-center items-center">
+            <AiOutlineLoading3Quarters className="animate-spin" />
           </div>
-          <div className="flex gap-4">
-            <p className="w-16 text-textColor-light">Phone</p>
-            <p>123-456-7800</p>
-          </div>
-          <div className="flex gap-4">
-            <p className="w-16 text-textColor-light">Vendor</p>
-            <p>XYZ Inc.</p>
-          </div>
-          <div className="flex gap-4">
-            <p className="w-16 text-textColor-light">Email</p>
-            <p>j.doe@gmail.com</p>
-          </div>
-          <div className="flex gap-4">
-            <p className="w-16 text-textColor-light">Address</p>
-            <p className={"p-ellipsis w-36"}>
-              1234 Fairy Ln, San Diego CA, 92110
-            </p>
-          </div>
-        </div>
+        ) : vendorDetails ? (
+          <section className="px-4 py-2 flex flex-col gap-2 w-80 h-52 3xl:h-56 border rounded">
+            <div className="flex gap-4 ">
+              <p className="w-16 text-textColor-light ">Contact</p>
+              <p>{vendorDetails.contact}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-16 text-textColor-light">Phone</p>
+              <p>{vendorDetails.phone}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-16 text-textColor-light">Vendor</p>
+              <p>{capitalizeFirstWord(vendorDetails.name)}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-16 text-textColor-light">Email</p>
+              <p>{vendorDetails.email}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="w-16 text-textColor-light">Address</p>
+              <p className={"p-ellipsis w-36"}>{vendorDetails.address}</p>
+            </div>
+          </section>
+        ) : (
+          <section className="h-52 3xl:h-56" />
+        )}
       </section>
       <Table data={data} />
-      <Button icon={MdPostAdd}>New Invoice</Button>
+      <Button icon={MdPostAdd} className={"my-2 3xl:my-4"}>
+        New Invoice
+      </Button>
     </section>
   );
 };
