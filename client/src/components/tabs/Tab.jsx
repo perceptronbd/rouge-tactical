83b3813883d532f23cb66c54cd382e7f363ab511
Asif Vudi <asifaslam.aa81@gmail.com>
2023-10-22 01:03:24 +0600

    - add a Manage Orders tab in orders page - the table shows the orders with approved: false - update approved items only show the items with approved:true

--- client/src/components/tabs/Tab.jsx
+++ client/src/components/tabs/Tab.jsx
@@ -5,7 +5,7 @@ export const Tab = ({ icon: Icon, label, isActive, onClick }) => {
   return (
     <button
       className={cw(
-        "flex justify-center items-center gap-4 text-textColor-light font-semibold px-4 py-2 rounded-t focus:outline-none hover:bg-gray-200 transition-all ease-in-out duration-300",
+        "flex justify-center items-center gap-4 text-textColor-light font-semibold px-4 py-2 rounded-t focus:outline-none hover:bg-gray-200 transition-all ease-in-out duration-300 rt-sm:text-xs",
         isActive
           ? "bg-foreground text-accent-tertiary hover:text-accent-hover"
           : "bg-opacity-0"
