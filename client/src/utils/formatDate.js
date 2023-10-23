01bd31c9029b31d3862d85f75266e8b446c807a9
Asif Vudi <asifaslam.aa81@gmail.com>
2023-10-23 20:29:35 +0600

    update formatDate for proper date format

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,23 +1,20 @@
 export const formatDate = (inputDate) => {
-  const parts = inputDate.split(/-|\//);
-  if (parts.length === 3) {
-    const month = new Date(parts[0], parts[1] - 1, parts[2]).toLocaleString(
-      "en-us",
-      {
-        month: "short",
-      }
-    );
-    const day = parseInt(parts[2]);
-    const daySuffix =
-      day === 1 || day === 21 || day === 31
-        ? "st"
-        : day === 2 || day === 22
-        ? "nd"
-        : day === 3 || day === 23
-        ? "rd"
-        : "th";
-    return `${month} ${day}${daySuffix} ${parts[0]}`;
-  } else {
+  const date = new Date(inputDate);
+
+  if (isNaN(date)) {
     return "Invalid Date";
   }
+
+  const month = date.toLocaleString("en-us", { month: "short" });
+  const day = date.getDate();
+  const daySuffix =
+    day === 1 || day === 21 || day === 31
+      ? "st"
+      : day === 2 || day === 22
+      ? "nd"
+      : day === 3 || day === 23
+      ? "rd"
+      : "th";
+
+  return `${month} ${day}${daySuffix} ${date.getFullYear()}`;
 };
