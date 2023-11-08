14b08cb0db68406fe65ae68261427533b06b1042
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-08 22:02:17 +0600

    Refactor date formatting logic in formatDate.js

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,22 +1,12 @@
 export const formatDate = (inputDate) => {
-  const parts = inputDate.split(/-|\//);
-  if (parts.length === 3) {
-    const month = new Date(parts[0], parts[1] - 1, parts[2]).toLocaleString(
-      "en-us",
-      {
-        month: "short",
-      }
-    );
-    const day = parseInt(parts[2]);
-    const daySuffix =
-      day === 1 || day === 21 || day === 31
-        ? "st"
-        : day === 2 || day === 22
-        ? "nd"
-        : day === 3 || day === 23
-        ? "rd"
-        : "th";
-    return `${month} ${day}${daySuffix} ${parts[0]}`;
+  const date = new Date(inputDate);
+
+  if (!isNaN(date)) {
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Adding 1 to month since it's zero-based
+    const day = date.getDate().toString().padStart(2, "0");
+    const year = date.getFullYear();
+
+    return `${month}-${day}-${year}`;
   } else {
     return "Invalid Date";
   }
