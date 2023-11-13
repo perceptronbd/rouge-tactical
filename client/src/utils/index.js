e70fb63ad83b3c9546cb8cd915d0d18f192f839e
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-14 03:03:01 +0600

    Add date formatting utility function to UpdateForm component

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,4 +2,5 @@ export { cw } from "./cw";
 export { capitalizeFirstLetter } from "./capitalize";
 export { capitalizeFirstWord } from "./capitalize";
 export { formatDate } from "./formatDate";
+export { formatDateToYYYYMMDD } from "./formatDate";
 export { parseDuration } from "./parseDuration";
