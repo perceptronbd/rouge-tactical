f37069200c8f65add9e3b21e0dc24972b338b5a4
Asif Vudi <asifaslam.aa81@gmail.com>
2023-11-17 02:03:13 +0600

    update logic for persanal, work email response

--- server/controller/auth/loginController.js
+++ server/controller/auth/loginController.js
@@ -10,20 +10,45 @@ const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Decrypt the password from the request body
-
+    // Check if the user exists with the provided preferredEmail
     const existingUser = await User.findOne({ preferredEmail: email });
-    console.log("existingUser", existingUser);
 
     if (!existingUser) {
-      res.status(401).json({ message: "User with such email doesn't exist" });
+      // Check if the user exists with the provided personalEmail
+      const personalEmail = await User.findOne({ personalEmail: email });
+
+      if (personalEmail) {
+        // Check if the personalEmail matches the provided email
+        if (personalEmail.personalEmail === email) {
+          return res
+            .status(401)
+            .json({ message: "Please provide personal email." });
+        }
+      }
+
+      // Check if the user exists with the provided workEmail
+      const workEmail = await User.findOne({ workEmail: email });
+
+      if (workEmail) {
+        // Check if the workEmail matches the provided email
+        if (workEmail.workEmail === email) {
+          return res
+            .status(401)
+            .json({ message: "Please provide work email." });
+        }
+      }
+
+      // If none of the conditions match, the user doesn't exist
+      return res
+        .status(401)
+        .json({ message: "User with such email doesn't exist" });
     } else {
       const hashedPassword = existingUser.password;
 
       // Compare the decrypted password with the stored hashed password
       bcrypt.compare(password, hashedPassword).then((match) => {
         if (!match) {
-          res
+          return res
             .status(400)
             .json({ message: "Wrong Email and Password Combination!" });
         } else {
@@ -32,7 +57,7 @@ const loginUser = async (req, res) => {
           // Set the access-token in the authorization header
           res.setHeader("Authorization", `Bearer ${accessToken}`);
 
-          res.status(200).json({
+          return res.status(200).json({
             token: accessToken,
             userData: existingUser,
             message: "User logged In successfully",
@@ -42,7 +67,7 @@ const loginUser = async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
