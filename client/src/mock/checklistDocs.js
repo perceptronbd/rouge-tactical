2ce99091655f505f8c89ebb4c6c9d0cd7c3f5170
ZBM Zubayer <zbmzubayer@gmail.com>
2023-12-16 00:40:07 +0600

    Mock data added for onboarding & checklist docs

---
+++ client/src/mock/checklistDocs.js
@@ -0,0 +1,37 @@
+export const checklistDocs = [
+  {
+    filename: "document1.doc",
+    uploadedBy: "John Doe",
+    date: "2022-01-01",
+  },
+  {
+    filename: "document2.doc",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.doc",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.doc",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.doc",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.doc",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document10.doc",
+    uploadedBy: "Alex Johnson",
+    date: "2022-01-10",
+  },
+];
---
+++ client/src/mock/onboardingDocs.js
@@ -0,0 +1,37 @@
+export const onboardingDocs = [
+  {
+    filename: "document1.pdf",
+    uploadedBy: "John Doe",
+    date: "2022-01-01",
+  },
+  {
+    filename: "document2.pdf",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.pdf",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.pdf",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.pdf",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document2.pdf",
+    uploadedBy: "Jane Smith",
+    date: "2022-01-02",
+  },
+  {
+    filename: "document10.pdf",
+    uploadedBy: "Alex Johnson",
+    date: "2022-01-10",
+  },
+];
